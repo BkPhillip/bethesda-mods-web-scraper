4330a26ca77e9841faede449b9bfa204449b17df
Bryson Phillip <bkphillip@icloud.com>
2022-11-27 00:29:39 -0800

    Bug Fixes

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ by Bryson Phillip
 11/22/22
 
 """
-# todo: Open filedialog for ability to name and choose location of saved file
-# todo: Time-period combo box
-# todo: Make Time-period combo box not appear when date published sorting option is selected
+# todo: Move app class to other file and import
 # todo: change number of ratings from k's to 1000's
 # todo: categories option from option box, "category=<category>&" inserted before "number_
 
@@ -24,10 +22,9 @@ CHROME_DRIVER_PATH = "/Users/BrysonPhillip/Development/chromedriver"
 class FalloutXboxModsScraper:
     def __init__(self, driver_path):
         self.driver = webdriver.Chrome(executable_path=driver_path)
-        self.url = "https://mods.bethesda.net/en/fallout4?number_results=&order=&page=1&platform=&product=&sort=&text="
+        self.url = "https://mods.bethesda.net/en/?number_results=&order=&page=1&platform=&product=&sort=&text="
 
-    def get_new_mods(self, quantity, order, console, game, sort, search_term):
-
+    def get_new_mods(self, save_path,  quantity, order, console, game, sort, period,  search_term):
         search_options = {"results=": quantity,
                           "order=": order,
                           "platform=": console,
@@ -36,12 +33,21 @@ class FalloutXboxModsScraper:
                           "text=": search_term}
 
         for option in search_options:
-            url_index = self.url.index(option) + len(option)  # get index after last character of string in URL
-            self.url = self.url[:url_index] + search_options[option] + self.url[url_index:]
+            if option == "product=":
+                url_index1 = self.url.index("/en/") + len("/en/")
+                url_index2 = self.url.index(option) + len(option)  # get index after last character of string in URL
+                self.url = self.url[:url_index1] + search_options[option] + self.url[url_index1:]
+                self.url = self.url[:url_index2] + search_options[option] + self.url[url_index2:]
+            elif option == "sort=" and period != "all time" and search_options[option] != "Date":
+                url_index = self.url.index(option) + len(option)  # get index after last character of string in URL
+                self.url = self.url[:url_index] + search_options[option] + f"-{period}" + self.url[url_index:]
+            else:
+                url_index = self.url.index(option) + len(option)  # get index after last character of string in URL
+                self.url = self.url[:url_index] + search_options[option] + self.url[url_index:]
 
         self.driver.get(self.url)
 
-        sleep(7)
+        sleep(7)  # Time for javascript to load
 
         mod_titles = [title.text for title in self.driver.find_elements(By.CSS_SELECTOR, "div.card-name p")]
         mod_authors = [author.text for author in self.driver.find_elements(By.CSS_SELECTOR, "div.card-user p")]
@@ -51,15 +57,13 @@ class FalloutXboxModsScraper:
                              self.driver.find_elements(By.CSS_SELECTOR, "div.card-rating span.rating-average-number")]
         mod_links = [link.get_attribute("href") for link in
                      self.driver.find_elements(By.CSS_SELECTOR, "div.content-module > a")]
-
         data = {"Name": mod_titles,
                 "Author": mod_authors,
                 "Rating": mod_ratings,
                 "Review Count": mod_review_counts,
                 "Link": mod_links}
-
         df = pd.DataFrame.from_dict(data)
-        df.to_csv("mod-data.csv")
+        df.to_csv(save_path)
 
         self.driver.quit()
 
@@ -78,8 +82,8 @@ class Window:
         asc_radio.pack()
         desc_radio = tk.Radiobutton(master, text="Descending", variable=self.order, value="desc")
         desc_radio.pack()
-        # Platform (XboxOne, Playstation4, PC)
-        platform_list = ["XB1", "PS4", "PC"]
+        # Platform (XboxOne, Playstation4, WINDOWS)
+        platform_list = ["XB1", "PS4", "WINDOWS"]
         self.platform_combobox = ttk.Combobox(master, values=platform_list)
         self.platform_combobox.current(0)
         self.platform_combobox.pack()
@@ -90,10 +94,15 @@ class Window:
         skyrim_radio = tk.Radiobutton(master, text="Skyrim", variable=self.game, value="skyrim")
         skyrim_radio.pack()
         # Sorting Options (date added, most popular, highest rated, most favorited)
-        sort_list = ["published", "popular", "rating", "follow"]
+        sort_list = ["Date", "Most Popular", "Highest Rated", "Most Favorited"]
         self.sort_combobox = ttk.Combobox(master, values=sort_list)
         self.sort_combobox.current(0)
         self.sort_combobox.pack()
+        # Time Period (daily, weekly, monthly, all time)
+        period_list = ["day", "week", "month", "all time"]
+        self.period_combobox = ttk.Combobox(master, values=period_list, state="disabled")
+        self.period_combobox.current(3)
+        self.period_combobox.pack()
         # Search Term
         search_label = tk.Label(master, text="Search:")
         search_label.pack()
@@ -103,18 +112,36 @@ class Window:
         button = tk.Button(master, text="Find Mods", command=self.create_csv)
         button.pack()
 
+        self.sort_combobox.bind('<<ComboboxSelected>>', self.change_sort)
         frame.pack(padx=10, pady=10, expand=True, fill=tk.BOTH)
 
     def create_csv(self):
+        save_path = fd.asksaveasfile(mode="w", initialfile="mod-data.csv", defaultextension=".csv")
+        # Get  widget values to make url search
         results = str(self.results.get())  # Get desired results quantity
         order = self.order.get()  # Ascending or Descending
         platform = self.platform_combobox.get()  # get console name
         game = self.game.get()  # Fallout 4 or Skyrim
-        sort = self.sort_combobox.get()  # Get sort choice from list
+        match self.sort_combobox.current():  # Get sort choice from list
+            case 0:
+                sort = "published"
+            case 1:
+                sort = "popular"
+            case 2:
+                sort = "rating"
+            case 3:
+                sort = "follow"
+        period = self.period_combobox.get()  # Get period choice from list
         search_term = self.search_entry.get()  # Get Search Term
 
         bot = FalloutXboxModsScraper(CHROME_DRIVER_PATH)
-        bot.get_new_mods(results, order, platform, game, sort, search_term)
+        bot.get_new_mods(save_path, results, order, platform, game, sort, period, search_term)
+
+    def change_sort(self, event):
+        if self.sort_combobox.get() == "Date":
+            self.period_combobox.config(state='disabled')
+        else:
+            self.period_combobox.config(state="normal")
 
 
 root = tk.Tk()
